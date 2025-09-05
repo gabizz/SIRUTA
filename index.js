72a411fa7597e770346d94756b85654786160438
Gabriel <gmaftei@gmail.com>
2025-09-05 20:49:17 +0300

    added rate limit via @fastify/rate-limit plugin - fix

--- index.js
+++ index.js
@@ -80,32 +80,35 @@ const resolvers = {
   },
 };
 
-const app = fastify({
-  logger: true,
-});
-
-app.register(rateLimit, {
-  max: 50, // maximum number of requests
-  timeWindow: '1 minute' // in a 1 minute window
-});
-
-app.register(mercurius, {
-  schema,
-  resolvers,
-  graphiql: true,
-});
-
-app.get('/api/searchLocalitati', async (req, reply) => {
-  const { name } = req.query;
-  if (!name) {
-    return reply.code(400).send({ error: 'Parametrul "name" este obligatoriu.' });
-  }
-  return searchLocalitatiLogic(name);
-});
-
+let app;
 
 // Export the Fastify instance for Vercel's runtime
 module.exports = async (req, res) => {
-  await app.ready();
+  if (!app) {
+    app = fastify({
+      logger: true,
+    });
+
+    app.register(rateLimit, {
+      max: 50, // maximum number of requests
+      timeWindow: '1 minute', // in a 1 minute window
+    });
+
+    app.register(mercurius, {
+      schema,
+      resolvers,
+      graphiql: true,
+    });
+
+    app.get('/api/searchLocalitati', async (req, reply) => {
+      const { name } = req.query;
+      if (!name) {
+        return reply.code(400).send({ error: 'Parametrul "name" este obligatoriu.' });
+      }
+      return searchLocalitatiLogic(name);
+    });
+
+    await app.ready();
+  }
   app.server.emit('request', req, res);
 };
